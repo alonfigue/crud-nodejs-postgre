910c347bc1f1338d68c00cfd1323d15eeec40566
alonfigue <alonsogfm2000@gmail.com>
2022-02-23 10:54:44 -0400

    working with the frontend

--- index.js
+++ index.js
@@ -27,43 +27,147 @@ app.get('/', (req, res) => {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD</title>
+    <title>CRUD-nodejs-postgresql</title>
+    <style>
+    #Titulo {
+        color: rgb(31, 31, 196);
+        font-family: Georgia, Verdana, sans-serif;
+        font-size: 250%;
+        text-align: center;
+        
+    }
+    
+    .red-button {
+        font-family: Arial, Helvetica, sans-serif;
+        background-color: #f44336; 
+        border-radius: 12px;
+        color: white;
+        padding: 13px 12px;
+        text-align: center;
+        font-size: 16px;
+        position: relative;
+        
+        margin: 4px 2px;
+        opacity: 0.6;
+        transition: 0.3s;
+        display: inline-block;
+        text-decoration: none;
+        cursor: pointer;
+        
+    }
+    
+    .red-button:hover {opacity: 1}
+    
+    .green-button {
+        text-align: center;
+        font-family: Arial, Helvetica, sans-serif;
+        background-color: #4CAF50; 
+        border-radius: 12px;
+        color: white;
+        padding: 13px 12px;
+        text-align: center;
+        font-size: 16px;
+        position: relative;
+        
+        margin: 4px 2px;
+        opacity: 0.6;
+        transition: 0.3s;
+        display: inline-block;
+        text-decoration: none;
+        cursor: pointer;
+        
+    }
+    
+    .green-button:hover {opacity: 1}
+    
+    .db-button {
+        position: absolute;
+        top: 40%;
+        width: 100%;
+        text-align: center;
+        font-size: 25px;
+    }
+    
+    .top-left-sided {
+        position: absolute;
+        top: 130px;
+        left: 16px;
+        font-size: 18px;
+        background-color: silver;
+    }
+
+    .top-right-sided {
+        position: absolute;
+        top: 100px;
+        right: 16px;
+        font-size: 18px;
+        background-color: silver;
+    }
+
+    .above-delete {
+        position: absolute;
+        bottom: 180px;
+        left: 20px;
+        font-size: 18px;
+        background-color: silver;
+    }
+    
+    .left-side {
+        position: absolute;
+        bottom: 75px;
+        left: 20px;
+        font-size: 18px;
+        background-color: silver;
+    }
+    
+    </style>
     </head>
     <body>
     
-    <form action="/info/add" method="POST">
-    <label for="add">AGREGAR PERSONA:</label>
-    <input type="text" name="nom" id="nom">
-    <input type="text" name="ap" id="ap">
-    <input type="text" name="ci" id="ci">
-    <input type="text" name="age" id="age">
-    <input type="text" name="gender" id="gender">
-    <input type="submit" value="CREATE">
+    <h1 id="Titulo">IU para hacer uso del CRUD</h1>
+    
+    <div class="top-left-sided">
+    <form action="/personas/create" method="POST">
+    <label for="create">AGREGAR PERSONA A LA BD:</label>
+    <input placeholder="Nombre" type="text" name="nom" id="nom">
+    <input placeholder="Apellido" type="text" name="ap" id="ap">
+    <input placeholder="Cedula" type="text" name="ci" id="ci">
+    <input placeholder="Edad" type="text" name="age" id="age">
+    <input placeholder="Genero" type="text"name="gender" id="gender">
+    <input class="red-button" type="submit" value="CREATE">
     </form>
+    </div>
     
-    <form action="/info/get" method="GET">
-    <input type="submit" value="VER DATOS DE LA BD (READ)">
+    
+    <div class="db-button">
+    <form action="/personas/get" method="GET">
+    <input class="green-button" type="submit" value="VER DATOS DE LA BD (READ)">
     </form>
+    </div>
     
-    
-    <form action="/info/update" method="POST">
-    <label for="oldCI">EDITAR DATOS DE UNA PERSONA (C.I.):</label>
-    <input type="text" name="oldCI" id="oldCI">
+
+    <div class="above-delete">
+    <form action="/personas/update" method="POST">
+    <label for="oldCI">EDITAR DATOS DE UNA PERSONA:</label>
+    <input placeholder="Introducir Cedula actual" type="text" name="oldCI" id="oldCI">
     <label for="newData">NUEVOS DATOS DE PERSONA:</label>
-    <input type="text" name="newNom" id="newNom">
-    <input type="text" name="newAp" id="newAp">
-    <input type="text" name="newCI" id="newCI">
-    <input type="text" name="newAge" id="newAge">
-    <input type="text" name="newGen" id="newGen">
-    <input type="submit" value="UPDATE">
+    <input placeholder="Nuevo Nombre" type="text" name="newNom" id="newNom">
+    <input placeholder="Nuevo Apellido" type="text" name="newAp" id="newAp">
+    <input placeholder="Nueva Cedula" type="text" name="newCI" id="newCI">
+    <input placeholder="Nueva Edad" type="text" name="newAge" id="newAge">
+    <input placeholder="Nuevo Genero" type="text" name="newGen" id="newGen">
+    <input class="red-button" type="submit" value="UPDATE">
     </form>
+</div>
     
     
-    <form action="/info/delete" method="POST">
-    <label for="delete">ELIMINAR PERSONA (C.I.):</label>
-    <input type="text" name="delete" id="delete">
-    <input type="submit" value="DELETE">
+    <div class="left-side">
+    <form action="/personas/delete" method="POST">
+    <label for="delete">ELIMINAR PERSONA:</label>
+    <input placeholder="Introducir Cedula" type="text" name="delete" id="delete">
+    <input class="red-button" type="submit" value="DELETE">
     </form>
+    </div>
     
     
     </body>
@@ -77,19 +181,19 @@ app.listen(port, () => {
 
 
 
-app.post('/info/add', (req, res) => {
+app.post('/personas/create', (req, res) => {
     try {
         pool.connect(async (error, client, release) => {
             let resp = await client.query(`INSERT INTO personas (Nombre, Apellido, Cedula, Edad, Sexo) VALUES ('${req.body.nom}', '${req.body.ap}', '${req.body.ci}', '${req.body.age}', '${req.body.gender}')`);
             release();
-            res.redirect('/info/get');
+            res.redirect('/personas/get');
         });
     } catch (error) {
         console.log(error)
     }
 });
 
-app.get('/info/get', (req, res) => {
+app.get('/personas/get', (req, res) => {
     try {
         pool.connect(async (error, client, release) => {
             let resp = await client.query(`SELECT * FROM personas`);
@@ -103,12 +207,12 @@ app.get('/info/get', (req, res) => {
 
 
 
-app.post('/info/update', (req, res) => {
+app.post('/personas/update', (req, res) => {
     try {
         pool.connect(async (error, client, release) => {
             let resp = await client.query(`UPDATE personas SET Nombre = '${req.body.newNom}', Apellido = '${req.body.newAp}', Cedula = '${req.body.newCI}', Edad = '${req.body.newAge}', Sexo = '${req.body.newGen}' WHERE Cedula = '${req.body.oldCI}'`);
             release();
-            res.redirect('/info/get');
+            res.redirect('/personas/get');
         });
     } catch (error) {
         console.log(error)
@@ -116,12 +220,12 @@ app.post('/info/update', (req, res) => {
 });
 
 
-app.post('/info/delete', (req, res) => {
+app.post('/personas/delete', (req, res) => {
     try {
         pool.connect(async (error, client, release) => {
             let resp = await client.query(`DELETE FROM personas WHERE Cedula = '${req.body.delete}'`);
             release();
-            res.redirect('/info/get');
+            res.redirect('/personas/get');
         });
     } catch (error) {
         console.log(error)
