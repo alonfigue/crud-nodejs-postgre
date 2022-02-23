f98c4da0e059eaa8fcd2dd573d2e7517c281237f
alonfigue <alonsogfm2000@gmail.com>
2022-02-23 07:32:13 -0400

    file .sql added

---
+++ datos.sql
@@ -0,0 +1,18 @@
+--Comandos para postgresql--
+
+
+-- BD Creada llamada "datos"
+create database datos;
+
+--Creada la tabla "Personas", perteneciente a la BD "datos"
+create table Personas (
+ID serial, 
+Nombre varchar(255) NOT NULL, 
+Apellido varchar(255) NOT NULL, 
+Cedula integer PRIMARY KEY, 
+Edad smallint NOT NULL CHECK (Edad > 0 AND Edad < 125), 
+Sexo varchar(255) NOT NULL
+);
+
+-- listar la data de la tabla "personas"
+select * from personas;
--- index.js
+++ index.js
@@ -45,6 +45,7 @@ app.get('/', (req, res) => {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD</title>
+    <link rel="stylesheet" href="styles.css"> 
     </head>
     <body>
     
